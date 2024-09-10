50bdfaea858439ae9d4529ab187ffdcaf3c9e472
wwonnn <108168611+wwonnn@users.noreply.github.com>
2024-09-10 16:54:32 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+    int x, y;
+    scanf("%d\n", &x);
+    scanf("%d", &y);
+
+    if (x > 0) {
+        if (y > 0) {
+            printf("1");
+        }
+        else if (y < 0) {
+            printf("4");
+        }
+    }
+    else if (x < 0) {
+        if (y > 0) {
+            printf("2");
+        }
+        else if (y < 0) {
+            printf("3");
+        }
+    }
+    else {
+
+    }
+
+    return 0;
+}
