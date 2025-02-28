ff1af6bcc879c3bc9c8f6f025eb6d114072afc8a
wwonnn <108168611+wwonnn@users.noreply.github.com>
2025-02-28 18:16:15 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	cout << "\\    /\\\n";
+	cout << " )  ( ')\n";
+	cout << "(  /  )\n";
+	cout << " \\(__)|";
+
+	return 0;
+}
