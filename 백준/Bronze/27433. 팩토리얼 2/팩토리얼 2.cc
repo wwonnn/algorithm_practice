e2dc5a3b92149f3c154dfb6158c1efc2bf32eeee
wwonnn <108168611+wwonnn@users.noreply.github.com>
2025-03-14 22:04:14 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+long long factorial(long long n) {
+    if (n == 1 || n ==  0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long n;
+    cin >> n;
+
+    cout << factorial(n);
+
+}
