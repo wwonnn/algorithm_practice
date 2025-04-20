9b6a5ed6503153e42fd07b6fea5a2083072736b5
wwonnn <108168611+wwonnn@users.noreply.github.com>
2025-04-20 17:16:15 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5816 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    int dp[1001][1001] = { 0 };
+
+    for (int i = 1; i <= s1.length(); i++) {
+        for (int j = 1; j <= s2.length(); j++) {
+            if (s1[i - 1] == s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+
+    cout << dp[s1.length()][s2.length()];
+}
