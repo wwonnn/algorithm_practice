054ac4a4283581fcad5d66691a3d93e9bdae3d79
wwonnn <108168611+wwonnn@users.noreply.github.com>
2025-06-23 23:26:35 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 92 ms, Memory: 11280 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+vector<int> adj[200001];
+bool vis[100001];
+int se[100001]; // 방문 순서
+
+bool comp(int a, int b) {
+    return a > b;
+}
+
+void bfs(int st) {
+    queue<int> q;
+    vis[st] = true;
+    se[st] = 1;
+    int count = 2;
+    q.push(st);
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        sort(adj[cur].begin(), adj[cur].end(), comp);
+        for (auto nxt : adj[cur]) {
+            if (!vis[nxt]) {
+                vis[nxt] = true;
+                se[nxt] = count;
+                count++;
+                q.push(nxt);
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m, r;
+    cin >> n >> m >> r;
+    
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    bfs(r);
+    for (int i = 1; i <= n; i++) {
+        cout << se[i] << "\n";
+    }
+
+}
