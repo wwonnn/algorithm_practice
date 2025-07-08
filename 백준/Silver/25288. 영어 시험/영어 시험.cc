b3f884915d0f54984fdd014d9de587318e20cedb
wwonnn <108168611+wwonnn@users.noreply.github.com>
2025-07-08 22:43:20 +0900

    [Silver IV] Title: 영어 시험, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/25288. 영어 시험/영어 시험.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+	int N; string s; cin >> N >> s;
+	while (N--) cout << s;
+
+}
